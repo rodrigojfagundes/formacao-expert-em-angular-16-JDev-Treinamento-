61359343d214f8a947afba66044384a4c7d572d5
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-19 20:28:30 -0300

    Variaveis Path API com Environment

---
+++ myApp/src/environments/environment.ts
@@ -0,0 +1,14 @@
+//definindo algumas variaveis globais de configurações
+
+export const environment = {
+  //url de onde ta exec o backend em java + spring
+  urlApi: 'https://www.cursojdev.projetojavaweb.com/loja_virtual_mentoria/',
+
+  //onde ta exec o backend java+spring local (obs se nao tiver pd ser a url do prof
+  //essa https://www.cursojdev.projetojavaweb.com/loja_virtual_mentoria/login)
+  //https://localhost:8443/loja_virtual_mentoria/ OU
+  //http://localhost:8080/loja_virtual_mentoria/
+
+  urlApiLocal: 'https://cursojdev.projetojavaweb.com/loja_virtual_mentoria/',
+  urlApiLocalSSL: 'https://localhost:8443/loja_virtual_mentoria/',
+};
