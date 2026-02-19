2abb2ace038e1e549f34afc403bb822211d147e0
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-19 20:46:28 -0300

    Criando logar no service

--- myApp/src/app/services/login.service.ts
+++ myApp/src/app/services/login.service.ts
@@ -2,11 +2,29 @@
 
 
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Usuario } from '../model/usuario';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor() {}
+  
+  private urlApi = environment.urlApi + 'login';
+
+  
+  constructor(private http: HttpClient) {}
+
+  
+  logar(usuario: Usuario) {
+    
+    
+    
+    
+    
+    
+    return this.http.post<String>(this.urlApi, usuario);
+  }
 }
