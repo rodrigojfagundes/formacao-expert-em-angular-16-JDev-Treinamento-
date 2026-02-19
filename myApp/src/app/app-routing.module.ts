08cecdca717c5476c11aea8338ca4717c5941187
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-19 20:26:15 -0300

    Declarando HTTPClient Module - Ajax

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +8,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
