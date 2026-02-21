0844c1f1eb6317d97f4b3d594866275357f94c6b
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-21 15:16:52 -0300

    Construindo o Model

--- myApp/src/app/model/cupom-desconto.ts
+++ myApp/src/app/model/cupom-desconto.ts
@@ -1,2 +1,12 @@
+import { PessoaJuridica } from './pessoa-juridica';
+
 export class CupomDesconto {
+  constructor() {}
+
+  id?: Number;
+  descricao?: String;
+  DataValidadeCupom?: Date;
+  valorRealDesc?: Number;
+  valorPorcentDesc?: Number;
+  empresa?: PessoaJuridica;
 }
