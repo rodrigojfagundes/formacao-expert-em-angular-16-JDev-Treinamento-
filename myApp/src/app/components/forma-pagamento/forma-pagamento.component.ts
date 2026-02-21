d4f3aa6da271bd92125ff77ceda5a88f047ed793
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-21 15:02:19 -0300

    Montando html

--- myApp/src/app/components/forma-pagamento/forma-pagamento.component.ts
+++ myApp/src/app/components/forma-pagamento/forma-pagamento.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormaPagamento } from 'src/app/model/forma-pagamento';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -8,10 +8,23 @@ import { LoginService } from 'src/app/services/login.service';
   templateUrl: './forma-pagamento.component.html',
   styleUrls: ['./forma-pagamento.component.css'],
 })
-export class FormaPagamentoComponent {
+export class FormaPagamentoComponent implements OnInit {
+  
+  lista = new Array<FormaPagamento>();
+  
+  FormPagProdForm: FormGroup;
+
   constructor(
     private fb: FormBuilder,
     private loginService: LoginService,
     private formaPagamento: FormaPagamento
-  ) {}
+  ) {
+    this.FormPagProdForm = this.fb.group({
+      id: [],
+      descricao: [null, Validators.required],
+      empresa: [this.loginService.objetoEmpresa(), Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
 }
