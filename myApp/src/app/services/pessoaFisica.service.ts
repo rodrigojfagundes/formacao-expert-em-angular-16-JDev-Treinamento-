cd41a6e68c44ced10ffa790191412866ffcac10c
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2026-02-21 14:13:16 -0300

    Service de usuarios no Angular

--- myApp/src/app/services/pessoaFisica.service.ts
+++ myApp/src/app/services/pessoaFisica.service.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { LoginService } from './login.service';
 import { PessoaFisica } from '../model/pessoa-fisica';
+import { PessoaFisicaComponent } from '../components/pessoa-fisica/pessoa-fisica.component';
 
 @Injectable({
   providedIn: 'root',
@@ -45,6 +46,26 @@ export class PessoaFisicaService {
         if (jsonResposta.error != undefined) {
           alert(jsonResposta.error);
         } else {
+          alert('Salvo com sucesso: ID: ' + jsonResposta.id);
+        }
+      },
+      error: (error) => {
+        console.info(error.error.error);
+        alert(error.error.error);
+      },
+    });
+  }
+
+  salvarpf2(m: PessoaFisica, c: PessoaFisicaComponent) {
+    return this.http.post<String>(this.urlApi + 'salvarPf', m).subscribe({
+      next: (res) => {
+        var varResposta = JSON.stringify(res);
+        var jsonResposta = JSON.parse(varResposta);
+
+        if (jsonResposta.error != undefined) {
+          alert(jsonResposta.error);
+        } else {
+          c.novo();
           alert('Salvo com sucesso: ID: ' + jsonResposta.id);
         }
       },
